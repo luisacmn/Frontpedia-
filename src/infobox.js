2fa1aa67fead059d62294bcaa88f1ce90280374b
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-04-04 16:04:58 -0300

    initial commit

--- src/infobox.js
+++ src/infobox.js
@@ -1,54 +1,91 @@
 const infobox = [
   {
-    id:1,
-    link:"www.google.com",
+    id: 1,
     img: "./images/googlefonts.png",
     name: "Google Fonts",
-    description: "Is a library of 1,364 free licensed font families and APIs for convenient use via CSS."
+    description:
+      "Is a library of 1,364 free licensed font families and APIs for convenient use via CSS.",
+    href: "https://fonts.google.com/"
   },
   {
-    id:2,
+    id: 2,
     img: "./images/fontawesome.png",
     name: "Font Awesome",
-    description:"Is the internet's icon library and toolkit used by millions of designers, developers, and content creators."
+    description:
+      "Is the internet's icon library and toolkit used by millions of designers, developers, and content creators.",
+    href: "https://fontawesome.com/"
   },
-  {  
-    id:3,
+  {
+    id: 3,
     img: "./images/colorhunt.png",
     name: "Color Hunt",
-    description: "Color Palettes for Designers and Artists. Get color inspiration for your design and art projects."
+    description:
+      "Color Palettes for Designers and Artists. Get color inspiration for your design and art projects.",
+    href: "https://colorhunt.co/"
   },
-  {  
-    id:4,
+  {
+    id: 4,
     img: "./images/cssgradient.png",
     name: "CSS Gradient",
-    description: "Gradients are CSS elements of the image data type that show a transition between two or more colors. "
+    description:
+      "Gradients are CSS elements of the image data type that show a transition between two or more colors. ",
+    href: "https://cssgradient.io/"
   },
-  {  
-    id:5,
+  {
+    id: 5,
     img: "./images/favicongenerator.png",
     name: "Favicon Generator",
-    description: "Generate your favicon from text, image, or choose from hundreds of emojis."
+    description:
+      "Generate your favicon from text, image, or choose from hundreds of emojis.",
+    href: "https://favicon.io/"
   },
-  {  
-    id:6,
+  {
+    id: 6,
     img: "./images/csstricky.png",
     name: "Flexbox Guide",
-    description: "This complete guide explains everything about CSS flexbox"
+    description: "This complete guide explains everything about CSS flexbox",
+    href: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/"
   },
-  {  
-    id:7,
+  {
+    id: 7,
     img: "./images/frontendmentor.png",
     name: "Frontend Mentor",
-    description: "Real-world HTML, CSS and JavaScript challenges to improve your front-end skills."
+    description:
+      "Real-world HTML, CSS and JavaScript challenges to improve your front-end skills.",
+    href: "https://www.frontendmentor.io/"
   },
-  {  
-    id:8,
+  {
+    id: 8,
     img: "./images/freefrontend.png",
     name: "Free Frontend",
-    description: "Free hand-picked HTML, CSS and JavaScript (jQuery, React, Vue) code examples, tutorials and articles."
+    description:
+      "Free hand-picked HTML, CSS and JavaScript (jQuery, React, Vue) code examples, tutorials and articles.",
+    href: "https://freefrontend.com/"
+  },
+  {
+    id: 9,
+    img: "./images/FreeCodeCamp.png",
+    name: "FreeCodeCamp",
+    description:
+      "Is a non-profit organization and integrated community that helps people learn to code for free.",
+    href: "https://www.freecodecamp.org/"
+  },
+  {
+    id: 10,
+    img: "./images/TheOdinProject.png",
+    name: "The Odin Project",
+    description:
+      "A platform that helps aspiring web developers to learn together for free.",
+    href: "https://www.theodinproject.com/"
+  },
+  {
+    id: 11,
+    img: "./images/ReactIcons.png",
+    name: "React Icons",
+    description:
+      "Include popular icons in your React projects easily with react-icons.",
+    href: "https://react-icons.github.io/react-icons/"
   }
-  
 ];
 
 export default infobox;
